fb15b7929ab97c57464d51160b7af989b9aac6dc
antropophagus <daniil.maltzeff@yandex.ru>
2018-04-15 04:20:06 +0300

    15.04.18 Upgrade slide №2

--- js/antroslider.js
+++ js/antroslider.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     for (var t = 1; t <= count; t++) {
         $('.buttons_slides').append('<li value="'+t+'" class="button_slide"></li>');
     }
-    $('.button_slide[value=1]').css({"backgroundColor": "black"});
+    $('.button_slide[value=1]').css({"backgroundColor": "white"});
     j = setInterval(function(){
         slideNext();
       },8000);
@@ -24,11 +24,11 @@ $(document).ready(function(){
       },8000);
     });
     $('.slides').hover(function(){
-        $('.antroslider_image_image p, h2').animate({"opacity":"1"},500);
-        $('.antroslider_image_image img').animate({"opacity":"0.35"},500);
+        $('.antroslider_image_image p, h1').animate({"opacity":"1"},{duration: 500, queue: false});
+        $('.antroslider_image_image img').animate({"opacity":"0.15"},{duration: 500, queue: false});
     },function(){
-        $('.antroslider_image_image p, h2').animate({"opacity":"0"},500);
-        $('.antroslider_image_image img').animate({"opacity":"1"},500);
+        $('.antroslider_image_image p, h1').animate({"opacity":"0"},{duration: 500, queue: false});
+        $('.antroslider_image_image img').animate({"opacity":"1"},{duration: 500, queue: false});
     });
     $(".button_next").click(function(){
         slideNext();
@@ -38,8 +38,8 @@ $(document).ready(function(){
     });
     $(".button_slide").click(function(){
         butt_id = $(this).index();
-        $(".button_slide").css({"backgroundColor": "white"});
-        $(".buttons_slides li[value="+(butt_id +1) +"]").css({"backgroundColor": "black"});
+        $(".button_slide").css({"backgroundColor": "black"});
+        $(".buttons_slides li[value="+(butt_id +1) +"]").css({"backgroundColor": "white"});
         i = butt_id + 1;
         a = -width * (i-1);
         $(".antroslider_image_image").animate({"left": a},"slow");
@@ -47,13 +47,13 @@ $(document).ready(function(){
 });
 function slideNext () { //Функция переключения на следующий слайд
     a += -width;
-    if (i == count) {slideFirstorLast();} else {$(".antroslider_image_image").animate({"left": a},"slow");i++;$(".button_slide").css({"backgroundColor": "white"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "black"})}
+    if (i == count) {slideFirstorLast();} else {$(".antroslider_image_image").animate({"left": a},"slow");i++;$(".button_slide").css({"backgroundColor": "black"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "white"})}
 }
 function slidePrev() {  //Функция переключения на предыдущий слайд
     a += width;
-    if (i == 1) {slideFirstorLast();} else {$(".antroslider_image_image").animate({"left": a},"slow");i--;$(".button_slide").css({"backgroundColor": "white"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "black"})}
+    if (i == 1) {slideFirstorLast();} else {$(".antroslider_image_image").animate({"left": a},"slow");i--;$(".button_slide").css({"backgroundColor": "black"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "white"})}
 }
 function slideFirstorLast() { //Функция переключения на первый или последний слайд
-    if (i == count) {a = 0; i = 1;$(".antroslider_image_image").animate({"left": a},"slow");$(".button_slide").css({"backgroundColor": "white"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "black"});}
-    else {a = -width * (count-1);i = count;$(".antroslider_image_image").animate({"left": a},"slow");$(".button_slide").css({"backgroundColor": "white"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "black"});}
+    if (i == count) {a = 0; i = 1;$(".antroslider_image_image").animate({"left": a},"slow");$(".button_slide").css({"backgroundColor": "black"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "white"});}
+    else {a = -width * (count-1);i = count;$(".antroslider_image_image").animate({"left": a},"slow");$(".button_slide").css({"backgroundColor": "black"});$(".buttons_slides li[value="+i+"]").css({"backgroundColor": "white"});}
 }
--- pages/index.php
+++ pages/index.php
@@ -7,17 +7,17 @@
         </div>
           <div class="antroslider">
             <div class="antroslider_window">
-                 <div class="button_next"><i class="fas fa-angle-right fa-3x"></i></div>
-                 <div class="button_prev"><i class="fas fa-angle-left fa-3x"></i></div>
+                 <button title="Следующий слайд" class="button_next button button-royal button-circle button-giant"><i class="fas fa-angle-right fa-2x"></i></button>
+                 <button title="Предыдущий слайд" class="button_prev button button-royal button-circle button-giant"><i class="fas fa-angle-left fa-2x"></i></button>
                  <ul class="buttons_slides">
                  </ul>
                <div class="slides">
-                    <a href="#1" title="first" class="antroslider_image"><div class="antroslider_image_image"><h2>lorem1</h2><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://i04.fotocdn.net/s23/90/public_pin_l/198/2579285337.jpg" alt="" ></div></a>
-                    <a href="#2" title="second" class="antroslider_image"><div class="antroslider_image_image"><h2>lorem2</h2><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://static.zerochan.net/Hatsune.Miku.full.2035781.jpg" alt="" ></div></a>
-                    <a href="#3" title="third" class="antroslider_image"><div class="antroslider_image_image"><h2>lorem3</h2><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://static.zerochan.net/Oginohama.Akane.full.1820022.jpg" alt="" ></div></a>
-                    <a href="#4" title="four" class="antroslider_image"><div class="antroslider_image_image"><h2>lorem4</h2><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="http://ii.yakuji.moe/s/src/1497530643255.png" alt="" ></div></a>
-                    <a href="#5" title="five" class="antroslider_image"><div class="antroslider_image_image"><h2>lorem5</h2><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://s1.1zoom.ru/big0/10/187720-dikemoon.jpg" alt="" ></div></a>
-                    <a href="#6" title="six" class="antroslider_image"><div class="antroslider_image_image"><h2>lorem6</h2><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="http://i.want.tf/to/that/DTP3xyrg.jpg" alt="" ></div></a>
+                    <a href="#1" title="first" class="antroslider_image"><div class="antroslider_image_image"><h1>lorem1</h1><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://i04.fotocdn.net/s23/90/public_pin_l/198/2579285337.jpg" alt="" ></div></a>
+                    <a href="#2" title="second" class="antroslider_image"><div class="antroslider_image_image"><h1>lorem2</h1><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://static.zerochan.net/Hatsune.Miku.full.2035781.jpg" alt="" ></div></a>
+                    <a href="#3" title="third" class="antroslider_image"><div class="antroslider_image_image"><h1>lorem3</h1><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://static.zerochan.net/Oginohama.Akane.full.1820022.jpg" alt="" ></div></a>
+                    <a href="#4" title="four" class="antroslider_image"><div class="antroslider_image_image"><h1>lorem4</h1><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="http://ii.yakuji.moe/s/src/1497530643255.png" alt="" ></div></a>
+                    <a href="#5" title="five" class="antroslider_image"><div class="antroslider_image_image"><h1>lorem5</h1><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="https://s1.1zoom.ru/big0/10/187720-dikemoon.jpg" alt="" ></div></a>
+                    <a href="#6" title="six" class="antroslider_image"><div class="antroslider_image_image"><h1>lorem6</h1><p>jewogwjegowiejgeoiwgjwoeigj</p><img src="http://i.want.tf/to/that/DTP3xyrg.jpg" alt="" ></div></a>
                </div>
             </div>
         </div>
