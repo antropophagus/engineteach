960b6454bb771732a333e2d4d0d583f2db6ff13a
antropophagus <daniil.maltzeff@yandex.ru>
2018-04-19 21:38:58 +0300

    19.04.18 Upgrade "creat state": Now you can download image from your url Also transfer JQuary and AJAX to different files!

--- admin/pages/user_manager.php
+++ admin/pages/user_manager.php
@@ -8,49 +8,8 @@
     <div id="wrapper" style="width: 40%;">
       <h2 style="margin-bottom: 15px;">Поиск пользователя: </h2>
       <input placeholder="Nickname" type="text" name="nickname"><p>или</p><input placeholder="Email" type="text" name="email"> <br>
-      <p><button name="search" class="button button-pill button-primary"><i class="fas fa-search"></i></button></p> <br><br>
+      <p><button name="search_state" class="button button-pill button-primary"><i class="fas fa-search"></i></button></p> <br><br>
     <div class="results"></div>
     </div>
   </body>
 </html>
-<script type="text/javascript">
-var myarray;
-function ShowUser() {
-  $.ajax ({
-    url: "/admin/manager",
-    type: "POST",
-    data: ({nickname: $("input[name=nickname]").val(), action: 'search', email: $("input[name=email]").val()}),
-    dataType: "html",
-    success: function(data) {
-      if(data == 'fail') {
-        var error = '<h2 style="color: red;">Пользователь не найден!</h2>';
-        $(".results").html(error);
-      }
-      else {
-        myarray = data.split ('&');
-        var user = '<p>ID: ' + myarray[0] + '</p><p>Email: ' + myarray[1] + '</p><p>Nickname: ' + myarray[2] + '</p><p>Reg.Date: ' + myarray[3] + '</p><p>Root: ' + myarray[4] + ' <select style="float: none;" id="root" name="root"><option disabled selected>root</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option></select></p>';
-        $(".results").html(user);
-      }
-      }
-  });
-}
-function ChangeUserRoot () {
-  $.ajax ({
-      url: "/admin/manager",
-      type: "POST",
-      data: ({root: $("#root").val(), action: 'change', id: myarray[0]}),
-      dataType: "html",
-      success: function(data) {
-        if (data == 'success') ShowUser();
-        else alert("Error!");
-      }
-  });
-}
-  $("button[name=search]").click(function(){
-    ShowUser();
-  });
-
-  $("#root").live('change', function(){
-    ChangeUserRoot();
-  });
-</script>
--- engine/functions.php
+++ engine/functions.php
@@ -50,6 +50,7 @@ function head() {
         <meta charset="utf-8">
         <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://engineteach.com/js/ajax.js"></script>
         <script type="text/javascript" src="http://engineteach.com/js/js.js"></script>
         <script type="text/javascript" src="http://engineteach.com/js/antroslider.js"></script>
         <title>Главная</title>
@@ -110,7 +111,7 @@ function ShowState($p1,$p2) {
     $Row = mysqli_fetch_assoc(mysqli_query($p1, "SELECT * FROM `states` WHERE `id` = '$p2'"));
     if (!$Row) exit('<h1>Статья не найдена!</h1>');
     echo '
-    <div class="content_wrapper"><h1>'.$Row["title"].'</h1><p>'.$Row["text"].'</p><span>'.$Row["create_date"].'</span><p>Tags: '.$Row["tags"].'</p></div>
+    <div class="content_wrapper"><h1>'.$Row["title"].'</h1><p><img src="http://engineteach.com/resources/images/'.$Row["image"].'" alt="" /></p><p>'.$Row["text"].'</p><span>'.$Row["create_date"].'</span><p>Tags: '.$Row["tags"].'</p></div>
     ';
 }
 
@@ -153,14 +154,89 @@ $q = mysqli_query($p1, "UPDATE `users` SET `root_rules` = $root WHERE `id` = $id
 if ($q) echo 'success';
 else echo 'error';
 }
-function createstate($p1, $title, $primary_text, $text, $category) {
+function createstate($p1, $title, $primary_text, $text, $category, $url) {
   $title = FormChars($title);
   $primary_text = FormChars($primary_text);
   $text = FormChars($text);
-  $Row = mysqli_query ($p1, "INSERT INTO `states` VALUES ('','$title','$primary_text','$text','$category', NOW())");
+  if ($url == '') $name = 'default_image.png';
+  else {
+// Проверим HTTP в адресе ссылки
+if (!preg_match("/^https?:/i", $url) && filter_var($url, FILTER_VALIDATE_URL)) {
+    die('Укажите корректную ссылку на удалённый файл.');
+}
+if (strpos($url, 'https://') !== false) $url = substr($url,8);
+
+// Запустим cURL с нашей ссылкой
+$ch = curl_init($url);
+// Укажем настройки для cURL
+curl_setopt_array($ch, [
+    // Укажем максимальное время работы cURL
+    CURLOPT_TIMEOUT => 60,
+    // Разрешим следовать перенаправлениям
+    CURLOPT_FOLLOWLOCATION => 1,
+    // Разрешим результат писать в переменную
+    CURLOPT_RETURNTRANSFER => 1,
+    // Включим индикатор загрузки данных
+    CURLOPT_NOPROGRESS => 0,
+    // Укажем размер буфера 1 Кбайт
+    CURLOPT_BUFFERSIZE => 1024,
+    // Напишем функцию для подсчёта скачанных данных
+    // Подробнее: http://stackoverflow.com/a/17642638
+    CURLOPT_PROGRESSFUNCTION => function ($ch, $dwnldSize, $dwnld, $upldSize, $upld) {
+        // Когда будет скачано больше 5 Мбайт, cURL прервёт работу
+        if ($dwnld > 1024 * 1024 * 5) {
+            return -1;
+        }
+    },
+    // Включим проверку сертификата (по умолчанию)
+    CURLOPT_SSL_VERIFYPEER => 1,
+    // Проверим имя сертификата и его совпадение с указанным хостом (по умолчанию)
+    CURLOPT_SSL_VERIFYHOST => 2,
+    // Укажем сертификат проверки
+    // Скачать: https://curl.haxx.se/docs/caextract.html
+    CURLOPT_CAINFO => __DIR__ . '/cacert.pem',
+]);
+$raw   = curl_exec($ch);    // Скачаем данные в переменную
+$info  = curl_getinfo($ch); // Получим информацию об операции
+$error = curl_errno($ch);   // Запишем код последней ошибки
+// Завершим сеанс cURL
+curl_close($ch);
+// Проверим ошибки cURL и доступность файла
+if ($error === CURLE_OPERATION_TIMEDOUT)  die('Превышен лимит ожидания.');
+if ($error === CURLE_ABORTED_BY_CALLBACK) die('Размер не должен превышать 5 Мбайт.');
+if ($info['http_code'] !== 200)           die('Файл не доступен.');
+// Создадим ресурс FileInfo
+$fi = finfo_open(FILEINFO_MIME_TYPE);
+// Получим MIME-тип используя содержимое $raw
+$mime = (string) finfo_buffer($fi, $raw);
+// Закроем ресурс FileInfo
+finfo_close($fi);
+// Проверим ключевое слово image (image/jpeg, image/png и т. д.)
+if (strpos($mime, 'image') === false) die('Можно загружать только изображения.');
+// Возьмём данные изображения из его содержимого
+$image = getimagesizefromstring($raw);
+// Зададим ограничения для картинок
+$limitWidth  = 1280;
+$limitHeight = 768;
+// Проверим нужные параметры
+if ($image[1] > $limitHeight) die('Высота изображения не должна превышать 768 точек.');
+if ($image[0] > $limitWidth)  die('Ширина изображения не должна превышать 1280 точек.');
+// Сгенерируем новое имя из MD5-хеша изображения
+$name = md5($raw);
+// Сгенерируем расширение файла на основе типа картинки
+$extension = image_type_to_extension($image[2]);
+// Сократим .jpeg до .jpg
+$format = str_replace('jpeg', 'jpg', $extension);
+// Сохраним картинку
+if (!file_put_contents(__DIR__ . '/../resources/images/' . $name . $format, $raw)) {
+    echo ('При сохранении изображения на диск произошла ошибка.');
+}
+  }
+  $Row = mysqli_query ($p1, "INSERT INTO `states` VALUES ('','$title','$primary_text','$text','$category', NOW(), '$name$format')");
   if (!$Row) echo 'fail';
   else echo 'success';
 }
+
 function search_state($p1, $title)
 {
   $title = FormChars($title);
